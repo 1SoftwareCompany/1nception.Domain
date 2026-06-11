061d365fe5ed274c6c4ee2000ed9d17204842563
Kalina Shupeva <kalina.shupeva@eldersoss.com>
2026-06-11 16:17:05 +0300

    feat: Adds ITracer interface for the purpose of tracking an action's origin (causation id, correlation id)

---
+++ src/One.Inception.Domain/ITracer.cs
@@ -0,0 +1,22 @@
+﻿namespace One.Inception;
+
+public interface ITracer
+{
+    void Record(string incomingMessageId, string correlationId = null);
+    TraceInfo GenerateTrace(string messageId = null);
+    TraceInfo GetTrace();
+}
+
+public record class TraceInfo
+{
+    public TraceInfo(string messageId, string causationId, string correlationId)
+    {
+        MessageId = messageId;
+        CausationId = causationId;
+        CorrelationId = correlationId;
+    }
+
+    public string MessageId { get; private set; }
+    public string CausationId { get; private set; }
+    public string CorrelationId { get; private set; }
+}
